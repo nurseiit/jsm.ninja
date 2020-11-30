90c424c1b81fecfc023ed49c28939ecf46ebb1c2
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-12-01 02:03:13 +0600

    ✨ Add basic counter

--- bot/index.js
+++ bot/index.js
@@ -27,14 +27,19 @@ const init = async () => {
 };
 
 const initWithUserId = async ({ id, first_name }) => {
-  await usersRef.add({
+  await usersRef.doc(`${id}`).set({
     booksStatus: [],
-    id,
     name: first_name,
     isAdmin: false,
     totalReadPages: 0,
   });
 };
+
+const numbersRegExp = new RegExp(/\d/g);
+
+const inputErrMsg = `💥 BOOM... 🔩☠🔧🔨⚡️
+Hmm, that wasn't supposed to happen. You didn't input invalid characters, or did you?
+`;
 
 const main = async () => {
   await init();
@@ -43,20 +48,43 @@ const main = async () => {
     const { first_name, id } = ctx.update.message.from;
     ctx.reply(`Welcome ${first_name}!`);
 
-    const snapshot = await usersRef.where('id', '==', id).get();
-    if (snapshot.empty) {
-      ctx.reply(`Initialising a new user.`);
-      console.log(`[INFO] Initialising a new user with id: ${id}.`);
-      try {
+    try {
+      const user = await usersRef.doc(`${id}`).get();
+      if (!user.exists) {
+        ctx.reply(`Initialising a new user.`);
+        console.log(`[INFO] Initialising a new user with id: ${id}.`);
         await initWithUserId({ id, first_name });
         ctx.reply(`✅ Done.`);
         console.log('[INFO] User initialised!');
-      } catch (e) {
-        ctx.reply(`❌ Could not initialise. Please contact admins!`);
-        console.error('[ERROR] Initialisation failed with: ', e);
+      } else {
+        ctx.reply(`Already initialised.`);
       }
-    } else {
-      ctx.reply(`Already initialised.`);
+    } catch (e) {
+      ctx.reply(`❌ Could not initialise. Please contact admins!`);
+      console.error('[ERROR] Initialisation failed with: ', e);
+    }
+  });
+
+  bot.hears(numbersRegExp, async (ctx) => {
+    try {
+      const pages = +ctx.message.text;
+      if (pages < 0) {
+        ctx.reply(inputErrMsg);
+      } else {
+        const { id } = ctx.update.message.from;
+        const userRef = usersRef.doc(`${id}`);
+        const user = await userRef.get();
+        const totalReadPages = user.data().totalReadPages + pages;
+        await userRef.update({
+          totalReadPages,
+        });
+        ctx.reply(
+          `👍 Updated. You've read a total of ${totalReadPages} pages!`
+        );
+      }
+    } catch (e) {
+      ctx.reply('❌ Could not update. Please, try again!');
+      console.error('[ERROR] Update pages failed with: ', e);
     }
   });
 
